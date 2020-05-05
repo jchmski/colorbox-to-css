5d82f3b4072c259765c427fd592f53b7f09fe5d2
John Chmilarski <jchmilarski@gmail.com>
2020-05-05 18:46:53 -0400

    layout updates

--- src/js/main.js
+++ src/js/main.js
@@ -17,9 +17,17 @@ document.querySelector('button').addEventListener('click', () => {
 
 	for (const color in colors) {
 		const div = document.createElement('div');
-		div.innerHTML = `<div>    --${prefix}-${(i++ * 100)}: ${colors[color]};</div>`;
+		div.innerHTML = `<div>    --${prefix}-${(i++ * 100)}: ${colors[color]}; <span style="background: ${colors[color]}"></span></div>`;
 		variables.appendChild(div);
 	}
 
 	variables.innerHTML += `<div>}</div>`;
+});
+
+document.querySelector('.copy').addEventListener('click', () => {
+	navigator.permissions.query({ name: "clipboard-write" }).then(result => {
+		if (result.state == "granted" || result.state == "prompt") {
+			navigator.clipboard.writeText(document.querySelector('.variables').innerText);
+		}
+	});
 });
